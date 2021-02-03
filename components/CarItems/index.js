689d2bef4361dd6a441b9c0ed385af0db80efeb9
sidaaryan7 <sidaaryan7@gmail.com>
2021-02-03 15:28:58 +0530

    2nd commit

--- components/CarItems/index.js
+++ components/CarItems/index.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import{View,Text,ImageBackground} from "react-native";
 import styles from "./styles";
+import StyledButton from "../StyledButton"
 
 export default function CarItem() {
     return (
         <View style={styles.carContainer}>
-        <ImageBackground source={require("../../assets/images/ModelX.jpeg")}
-        style={styles.image}/>
-        <View style={styles.titles}>
+            <ImageBackground source={require("../../assets/images/ModelX.jpeg")}
+            style={styles.image}/>
+            <View style={styles.titles}>
           <Text style={styles.title}>Model s</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
         </View>
+        <StyledButton type="primary" content={"Custom Ordered"} onPress={()=>{
+            console.warn("custom ordered was pressed")
+        }}/>
+
+        <StyledButton
+        type="secondary"
+        content={"existing inventory"}
+        onPress={()=>{
+            console.warn("existing inventory was press")
+        }}
+        />
       </View>
     )
 }
