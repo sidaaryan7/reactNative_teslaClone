9ceeb3fc10c4d8e6fa601252361b9df6d9f1f6d8
sidaaryan7 <sidaaryan7@gmail.com>
2021-02-03 14:42:48 +0530

    first commit

---
+++ components/CarItems/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import{View,Text,ImageBackground} from "react-native";
+import styles from "./styles";
+
+export default function CarItem() {
+    return (
+        <View style={styles.carContainer}>
+        <ImageBackground source={require("../../assets/images/ModelX.jpeg")}
+        style={styles.image}/>
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model s</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>
+      </View>
+    )
+}
